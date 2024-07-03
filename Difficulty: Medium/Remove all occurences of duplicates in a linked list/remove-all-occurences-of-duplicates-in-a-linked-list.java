dc4c37d310e51b0206fce138ce8c3b91b85a3647
Ayush Nandi <110190468+ayushnandi@users.noreply.github.com>
2024-07-03 10:19:22 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Remove all occurences of duplicates in a linked list/remove-all-occurences-of-duplicates-in-a-linked-list.java
@@ -0,0 +1,83 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+
+class GFG {
+    Node head;
+    Node tail;
+
+    public void addToTheLast(Node node) {
+        if (head == null) {
+            head = node;
+            tail = node;
+        } else {
+            tail.next = node;
+            tail = node;
+        }
+    }
+
+    void printList(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    /* Drier program to test above functions */
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t > 0) {
+            int N = sc.nextInt();
+            GFG llist = new GFG();
+            int a1 = sc.nextInt();
+            Node head = new Node(a1);
+            llist.addToTheLast(head);
+            for (int i = 1; i < N; i++) {
+                int a = sc.nextInt();
+                llist.addToTheLast(new Node(a));
+            }
+
+            Solution ob = new Solution();
+            head = ob.removeAllDuplicates(llist.head);
+            llist.printList(head);
+
+            t--;
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    public Node removeAllDuplicates(Node head) {
+        // code here
+        HashMap<Integer,Integer> map = new HashMap<>(); 
+        Node n1 = head;
+        while(head!=null){
+            map.put(head.data,map.getOrDefault(head.data,0)+1);
+            head = head.next;
+        }
+        Node n2 = new Node(0);
+        Node n3 = n2;
+        while(n1 != null ){
+            if(map.get(n1.data)<2){
+                n2.next = new Node(n1.data);
+                n2 = n2.next;
+            }
+            n1 = n1.next;
+        }
+        return n3.next;
+    }
+}
