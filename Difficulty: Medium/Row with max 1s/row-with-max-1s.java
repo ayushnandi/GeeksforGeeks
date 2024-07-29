d55f364e149181c9fd7fafde042b04705cc25e92
Ayush Nandi <110190468+ayushnandi@users.noreply.github.com>
2024-07-29 15:27:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Row with max 1s/row-with-max-1s.java
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            inputLine = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+            int m = Integer.parseInt(inputLine[1]);
+            int[][] arr = new int[n][m];
+            inputLine = br.readLine().trim().split(" ");
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    arr[i][j] = Integer.parseInt(inputLine[i * m + j]);
+                }
+            }
+            int ans = new Solution().rowWithMax1s(arr);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+     public int helper(int[] num) {
+        int count = 0;
+        for (int i : num) {
+            if (i == 1) count++;
+        }
+        return count;
+    }
+
+    public int rowWithMax1s(int arr[][]) {
+        int maxRowIndex = -1;
+        int maxCount = 0;
+        for (int i = 0; i < arr.length; i++) {
+            int count = helper(arr[i]);
+            if (count > maxCount) {
+                maxCount = count;
+                maxRowIndex = i;
+            }
+        }
+        return maxRowIndex;
+    }
+}
